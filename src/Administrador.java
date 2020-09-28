abdb342e019690eb6c9f5a8c4c93f4f5509dadf0
dtorresag <55595673+dtorresag@users.noreply.github.com>
2020-09-28 10:14:18 -0500

    clases

--- src/Administrador.java
+++ src/Administrador.java
@@ -1,4 +1,19 @@
+import java.util.ArrayList;
 
-public class Administrador {
-
+public class Administrador extends Persona {
+	private double salario;
+	private String profesion;
+	static private ArrayList<Administrador>administradores;
+	
+	public double getSalario() {
+		return salario;
+	}
+	public String getProfesion() {
+		return profesion;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	
 }
