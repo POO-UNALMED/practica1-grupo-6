add5788003dd0ae7b2dba2b6e7a558ab670863ce
DeninsonCH <69688198+DeninsonCH@users.noreply.github.com>
2020-09-28 10:18:05 -0500

    datos clases
    ...sin definir codigos de metodos

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,4 +1,18 @@
 
 public class Cliente {
-
+	private int compras;
+	private ArrayList<String> lista_facturas = new ArrayList<String>();
+	
+	public int selec_producto(String p) {
+		/*tomar producto*/
+	}
+	public int registro(int r) {
+		/*buscar producto*/
+	}
+	public boolean login(String user, int contra) {
+		/*verificacion true o false*/
+	}
+	public int consultar_producto(String producto) {
+		/*buscar producto*/
+	}
 }
--- src/Pedido.java
+++ src/Pedido.java
@@ -1,4 +1,22 @@
 
 public class Pedido {
-
+	private int cantidad_comprar;
+	private ArrayList<String> productos = new ArrayList<String>();
+	private int num_pedido;
+	private boolean estado;
+	private int totalPago;
+	
+	
+	public int calcular_precio(String p) {
+		/*tomar producto*/
+	}
+	public int Agregar_producto(String a) {
+		/*buscar producto*/
+	}
+	public int quitar_producto(String q) {
+		/*buscar producto*/
+	}
+	public int totalPago(int productos, int cantidad) {
+		/*buscar producto*/
+	}
 }
