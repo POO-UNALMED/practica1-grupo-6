b65681c9725d32d303c4d9af4ad567074dde160c
DeninsonCH <69688198+DeninsonCH@users.noreply.github.com>
2020-09-28 10:50:39 -0500

    eliminacion de login

--- src/Cliente.java
+++ src/Cliente.java
@@ -3,18 +3,20 @@ import java.util.ArrayList;
 public class Cliente {
 	private int compras;
 	private ArrayList<String> lista_facturas = new ArrayList<String>();
+	private ArrayList<String> loginUser = new ArrayList<String>();
 	
 	public int selec_producto(String p) {
 		/*tomar producto*/
 	}
-	public int registro(int r) {
-		/*buscar producto*/
-	}
-	public boolean login(String user, int contra) {
-		/*verificacion true o false*/
-	}
 	public String consultar_producto(String producto) {
-		lista_facturas.contains(producto);
-		return producto;
+		String no="no esta";
+		String si="si esta";
+		
+		if (lista_facturas.contains(producto)==true) {
+			
+			return si;
+		}else
+			return no;
+		
 	}
 }
