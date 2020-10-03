f79e19ff6edba8d549423fac3836860c495e5738
dtorresag <55595673+dtorresag@users.noreply.github.com>
2020-10-03 18:29:56 -0500

    factura

--- src/Factura.java
+++ src/Factura.java
@@ -1,6 +1,24 @@
+import java.util.ArrayList;
 
 public class Factura {
-	private int Fecha;
-	private int Nfactura=110;
+	private String Fecha;
+	private int Nfactura;
+	private String nombreCliente;
+	private static int Numfactura=110;
+	private Pedido pedido;
+	private static ArrayList<Factura> Facturas = new ArrayList<Factura>();
+	
+	public Factura( String Fecha,String nombreCliente, Pedido pedido) {
+		super();
+		Nfactura = Numfactura;
+		this.Fecha=Fecha;
+		this.nombreCliente = nombreCliente;
+		this.pedido = pedido;
+		Numfactura++;
+		Facturas.add(this);
+	}
+	
+	
+	
 	
 }
